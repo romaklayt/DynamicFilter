980f77144f54670d797fe03a93ef12f184538090
Roma Kolyago <romakolyago18@gmail.com>
2021-09-09 22:20:14 +0300

    Revert "del trash"
    This reverts commit e39421c820f92d989fc1475c4a45c93aefdf230e.

---
+++ romaklayt.DynamicFilter.Test.Api.NetFramework/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace romaklayt.DynamicFilter.Test.Api.NetFramework
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "User", action = "GetList", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
