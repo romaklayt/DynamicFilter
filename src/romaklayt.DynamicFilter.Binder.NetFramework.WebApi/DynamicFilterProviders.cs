7b8bd2b53c8410710e27fd0add69756644a0ad26
Roma Kolyago <romakolyago18@gmail.com>
2022-03-27 18:19:37 +0300

    added missing providers for .net framework webapi

--- src/romaklayt.DynamicFilter.Binder.NetFramework.WebApi/DynamicFilterProviders.cs
+++ src/romaklayt.DynamicFilter.Binder.NetFramework.WebApi/DynamicFilterProviders.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Http.ValueProviders;
+using System.Web.Http.ValueProviders.Providers;
 using romaklayt.DynamicFilter.Binder.NetFramework.WebApi.Factories;
 
 namespace romaklayt.DynamicFilter.Binder.NetFramework.WebApi;
@@ -11,5 +12,7 @@ public static class DynamicFilterProviders
         config.Services.Add(typeof(ValueProviderFactory), new FormValueProviderFactory());
         config.Services.Add(typeof(ValueProviderFactory), new MultipartValueProviderFactory());
         config.Services.Add(typeof(ValueProviderFactory), new JsonBodyValueProviderFactory());
+        config.Services.Add(typeof(ValueProviderFactory), new QueryStringValueProviderFactory());
+        config.Services.Add(typeof(ValueProviderFactory), new RouteDataValueProviderFactory());
     }
 }
